6e621eed14b23151f832fb8c73a48adfd7443ced
Byeonghun Song <sbh3003@gmail.com>
2026-01-16 23:04:36 +0900

    Setup GO app and CI pipeline

---
+++ app/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	hostname, _ := os.Hostname()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello K8s! Response from Pod: %s\n", hostname)
+	})
+
+	fmt.Println("Server starting on port 8080...")
+	http.ListenAndServer(":8080", nil)
+}
